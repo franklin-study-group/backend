519e580ffb40cc602c909ee39dce8d97ada2348e
studybuddies-git <76235679+studybuddies-git@users.noreply.github.com>
2021-02-25 16:46:25 -0800

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express"
 import { userRouter } from "./users"
+import { mailerRouter } from "./mailer"
 
 const router = Router()
 
 router.use("/users", userRouter)
+router.use("/mailer", mailerRouter)
 
 export const masterRouter = router
+export const emailRouter = router;
